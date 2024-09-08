1cb47eaabbeaa931fc4062218975c113682d285f
Константин <zed726804@yandex.ru>
2024-09-08 10:01:41 +0300

    Initialization of project

---
+++ src/main/java/com/scienceentertainment/scienceentertainment/ScienceentertainmentApplication.java
@@ -0,0 +1,13 @@
+package com.scienceentertainment.scienceentertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceentertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceentertainmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scienceentertainment/scienceentertainment/ScienceentertainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scienceentertainment.scienceentertainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceentertainmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
